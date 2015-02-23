2d0bc92b76570ee981bd6f4bf1e870de21bd0d4d
kngeno <kngeno@ymail.com>
2015-02-23 16:51:58 +0300

    static directory change & wsgi

--- upande/wsgi.py
+++ upande/wsgi.py
@@ -14,6 +14,6 @@ from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 from whitenoise.django import DjangoWhiteNoise
 
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+#application = get_wsgi_application()
+#application = DjangoWhiteNoise(application)
 application = Cling(get_wsgi_application())
